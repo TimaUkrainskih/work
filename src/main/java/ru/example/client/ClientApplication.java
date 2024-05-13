73286d84e88cb8aba815c4c5ad650e6ab383db9b
TimaUkrainskih <timofej.nikitan@mail.ru>
2024-05-13 13:47:39 +0300

    Retryable

--- src/main/java/ru/example/client/ClientApplication.java
+++ src/main/java/ru/example/client/ClientApplication.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+@EnableRetry
 @SpringBootApplication
 public class ClientApplication {
 
--- src/main/java/ru/example/client/repository/ClientsJDBCRepository.java
+++ src/main/java/ru/example/client/repository/ClientsJDBCRepository.java
@@ -1,7 +1,11 @@
 package ru.example.client.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Repository;
 import ru.example.client.mapper.ClientsAllInfoRowMapper;
 import ru.example.client.model.ClientsAllInfo;
@@ -15,7 +19,10 @@ public class ClientsJDBCRepository implements ClientsRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
+
     @Override
+    @Retryable(retryFor = {DataAccessException.class, CannotGetJdbcConnectionException.class},
+            maxAttempts = 7, backoff = @Backoff(delay = 500))
     public List<ClientsAllInfo> clients(int offset) {
         String sql = "select clients.client_id,name,age,email,subscription_type,price" +
                 " from clients" +
